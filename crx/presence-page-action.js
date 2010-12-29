a09b1c867f3a511d5a6aa6edcabcdf2d2026db11
Johan Sundström <oyasumi@gmail.com>
2010-12-28 19:57:13 -0800

    v1.2: Click the page action icon to toggle the banner.
    Also, record how many of those we have removed across all the wikimedia sites.

--- crx/presence-page-action.js
+++ crx/presence-page-action.js
@@ -2,11 +2,22 @@ var site_notice = document.getElementById('siteNotice');
 if (site_notice)
   site_notice.addEventListener('DOMSubtreeModified', changed, false);
 
+// await the banner's jQuery-mediated insertion, and then:
 function changed(e) {
+  function toggle() {
+    banner.style.display = banner.style.display ? '' : 'block !important';
+  }
+
+  function pageActionClick(req, from, sendResponse) {
+    if (!from.tab) toggle(); // only react to the background page
+  }
+
   var banner = site_notice.querySelector('*[id*=Banner]');
   // console.warn('Site notice changed; banner:', banner);
-  if (banner) {
+
+  if (banner) { // banner confirmed; stop waiting and pop the page action icon
     site_notice.removeEventListener('DOMSubtreeModified', changed, false);
     chrome.extension.sendRequest({}, function(response) {});
+    chrome.extension.onRequest.addListener(pageActionClick);
   }
 }
